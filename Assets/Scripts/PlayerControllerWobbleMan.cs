ea1dc986c1e226b144316daea07456544fa2ce62
mickey kawick <mkawick@yahoo.com>
2020-06-15 15:56:50 +0100

    rehook the new fireman: victims follow player, player does not fall over

--- Assets/Scripts/PlayerControllerWobbleMan.cs
+++ Assets/Scripts/PlayerControllerWobbleMan.cs
@@ -12,11 +12,36 @@ public class PlayerControllerWobbleMan : MonoBehaviour
     bool isMouseHeld = false;
     BasicPeepAnimController control;
     public float minimalRangeCheckToMove = 0.01f;
+    bool isButtonHeld = false;
     private void Start()
     {
         control = GetComponent<BasicPeepAnimController>();
     }
     private void Update()
+    {
+        isButtonHeld = Input.GetMouseButton(0);
+
+       /* if (isButtonHeld == true)
+        {
+            if (isMouseHeld == true)
+            {
+                Vector3 vect = GetFingerPosition();
+                MoveInDirection(vect);
+            }
+            else
+            {
+                lastFingerPosition = GetFingerPosition();
+                isMouseHeld = true;
+            }
+        }
+        else
+        {
+            isMouseHeld = false;
+            StopInPlace();
+        }*/
+    }
+
+    private void FixedUpdate()
     {
         bool isButtonHeld = Input.GetMouseButton(0);
 
@@ -25,6 +50,7 @@ public class PlayerControllerWobbleMan : MonoBehaviour
             if (isMouseHeld == true)
             {
                 Vector3 vect = GetFingerPosition();
+                Debug.Log(vect);
                 MoveInDirection(vect);
             }
             else
@@ -42,6 +68,7 @@ public class PlayerControllerWobbleMan : MonoBehaviour
 
     void MoveInDirection(Vector3 vect)
     {
+        vect.y = 0;// this.transform.position.y;
         if ((vect - lastFingerPosition).magnitude < minimalRangeCheckToMove) // todo, turn into a range check... slow moving or small movements should be ignored
         {
             StopInPlace();
